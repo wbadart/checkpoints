01919b2ec27344d2033794ab7f9bac4fd3789049
Will Badart <badart_william@bah.com>
2020-03-23 21:58:02 -0700

    Added setup.py
    Added installation instructions to README.

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # checkpoints
+
 Cache results of functions returning pandas DataFrames.
+
+## Installation
+
+Add to the current Python site packages by running
+
+```
+$ pip install git+ssh://git@github.boozallencsn.com/Badart-William/checkpoints.git
+```
+
+Or add
+`git+ssh://git@github.boozallencsn.com/Badart-William/checkpoints.git`
+to the `install_requires` setting of your `setup.py` file or to your
+`requirements.txt` file.
---
+++ setup.py
@@ -0,0 +1,31 @@
+"""setup.py
+
+Package configuration and dependencies.
+
+Will Badart <Badart_William@bah.com>
+created: MAR 2020
+"""
+
+from setuptools import setup
+
+with open('./README.md') as fs:
+    long_description = fs.read()
+
+setup(name='checkpoints',
+      version='1.0.0',
+      author='Will Badart',
+      author_email='Badart_William@bah.com',
+      description='Cache results of functions returning pandas DataFrames.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.boozallencsn.com/Badart-William/checkpoints',
+      py_modules=['checkpoints'],
+
+      install_requires=[
+          'pandas',
+      ],
+
+      project_urls={
+          'Bug Reports': 'https://github.boozallencsn.com/Badart-William/checkpoints/issues',
+      },
+     )
